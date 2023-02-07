dfbc8fd1d2966d039f3f8f2909a3d454d9022263
arnaucube <root@arnaucube.com>
2023-02-07 18:02:48 +0100

    clientlib: add nullifier & buildVote package
    Add nullifier & buildVote methods with simple tests, the missing part is
    the zk-proof generation and the Metamask key derivation.

--- clientlib/src/main.js
+++ clientlib/src/main.js
@@ -19,9 +19,61 @@ class AnonVote {
 		this.nLevels = nLevels
 		this.web3gw = web3gw; // optional
 	}
-	generateKey() {}
-	computeNullifier(processID) {}
-	buildVote(processID, proof, vote) {}
+
+	generateKey() {
+		// TODO derive from Metamask signature
+	}
+
+	computeNullifier(processID) {
+		const nullifier = this.poseidon([
+			this.chainID,
+			processID,
+			this.publicKey[0],
+			this.publicKey[1],
+		]);
+		return this.F.toObject(nullifier).toString();
+	}
+
+	buildVote(processID, provingKey, censusRoot, proof, publicKey, vote) {
+		// for this version, weight is hardcoded to 1 for all voters
+		const weight = "1";
+
+		const nullifier = this.computeNullifier(processID);
+
+		// compute signature
+		const toSign = this.poseidon([vote]);
+		const signature = this.eddsa.signPoseidon(this.privateKey, toSign);
+
+		// set the zk-inputs
+		const inputs = {
+			chainID: this.chainID, // public inputs
+			processID: processID,
+			censusRoot: censusRoot,
+			weight: weight,
+			nullifier: nullifier,
+			vote: vote,
+			index: proof.index, // private inputs
+			pubKx: this.F.toObject(publicKey[0]).toString(),
+			pubKy: this.F.toObject(publicKey[1]).toString(),
+			s: signature.S,
+			rx: this.F.toObject(signature.R8[0]).toString(),
+			ry: this.F.toObject(signature.R8[1]).toString(),
+			siblings: proof.siblings,
+		};
+
+		const publicInputs = {
+			chainID: this.chainID,
+			processID: processID,
+			censusRoot: censusRoot,
+			weight: weight,
+			nullifier: nullifier,
+			vote: vote
+		};
+
+		// TODO generate zk-proof
+		// TODO return public inputs + zk-proof
+		return {publicInputs: publicInputs};
+	}
 
 	// methods containing the logic of interacting with SmartContracts &
 	// servers. Only available if web3gateway is defined.
@@ -36,16 +88,14 @@ class AnonVote {
 
 async function buildCensus(nLevels) {
 	const poseidon = await buildPoseidonReference();
-	const eddsa = await buildEddsa();
 	const tree = await newMemEmptyTrie()
-	return new Census(tree, poseidon, eddsa, nLevels);
+	return new Census(tree, poseidon, nLevels);
 }
 
 class Census {
-	constructor(tree, poseidon, eddsa, nLevels) {
+	constructor(tree, poseidon, nLevels) {
 		this.poseidon = poseidon;
 		this.F = this.poseidon.F;
-		this.eddsa = eddsa;
 		this.nLevels = nLevels
 		this.tree = tree;
 	}
@@ -74,6 +124,10 @@ class Census {
 			value: this.F.toObject(res.foundValue).toString(),
 			proof: siblings
 		};
+	}
+
+	root() {
+		return this.F.toObject(this.tree.root).toString();
 	}
 
 }
