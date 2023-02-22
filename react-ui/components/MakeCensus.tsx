7b4f2245cf5b78c9595518516da19a1a14e66312
Craig <craig>
2023-02-22 13:42:38 +0100

    Added clientlib and associated settings

--- react-ui/components/MakeCensus.tsx
+++ react-ui/components/MakeCensus.tsx
@@ -1,7 +1,28 @@
+import { Census, buildCensus } from "../hooks/census";
+
 export default function MakeCensus() {
+
+	const createCensus = async () => {
+		const nLevels = 16;
+		try {
+			let census = await buildCensus(nLevels);
+	
+			let keyList = document.getElementById('keylist').value;
+			let keyArray = keyList.split('\n');
+
+			await census.addKeys(keyArray);
+
+			let proof = await census.generateProof(0);
+	
+			document.getElementById('census-root').value = proof.censusRoot;
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
 	function copyRoot() {
 		// Get the text field
-		var copyText = document.getElementById("merkel-root");
+		var copyText = document.getElementById("census-root");
 
 		// Select the text field
 		copyText.select();
@@ -17,7 +38,7 @@ export default function MakeCensus() {
       <div>
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
-			<div className="sm:max-w-lg">
+			<div className="shadow sm:max-w-lg sm:rounded-md px-4 py-5 bg-white bg-opacity-50">
             <h3 className="font text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Build the vote census
             </h3>
@@ -27,7 +48,7 @@ export default function MakeCensus() {
           </div>
           </div>
           <div className="mt-5 md:col-span-2 md:mt-0">
-            <form action="#" method="POST">
+			<form onSubmit={(event) => event.preventDefault()}>
               <div className="shadow sm:overflow-hidden sm:rounded-md">
                 <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
 
@@ -43,7 +64,7 @@ export default function MakeCensus() {
                     <div className="mt-1">
                       <textarea
                         id="keylist"
-                        name="about"
+                        name="keylist"
                         rows={12}
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         defaultValue={''}
@@ -55,20 +76,20 @@ export default function MakeCensus() {
                   </div>
 
                   <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label htmlFor="merkel-root" className="block text-sm font-medium text-gray-700">
-                        Merkel Root
+                    <div className="col-span-6">
+                      <label htmlFor="census-root" className="block text-sm font-medium text-gray-700">
+                        Census Merkel Root
                       </label>
                       <div className="mt-1 flex rounded-md shadow-sm">
                         <span onClick={copyRoot} style={{cursor: 'grab'}} className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
 </svg>
                         </span>
                         <input
                           type="text"
-                          name="merkel-root"
-                          id="merkel-root"
+                          name="census-root"
+                          id="census-root"
                           className="block w-full flex-1 rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         />
                       </div>
@@ -77,14 +98,14 @@ export default function MakeCensus() {
                 </div>
                 <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
                   <button
-                    type="submit"
+					onClick={ createCensus }
                     className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                   >
                     Create Census
                   </button>
                 </div>
               </div>
-            </form>
+			</form>
           </div>
         </div>
       </div>
---
+++ react-ui/hooks/anonvote.js
@@ -0,0 +1,262 @@
+import { buildPoseidonReference, buildEddsa, buildBabyjub } from "circomlibjs",
+import { utils } from 'ffjavascript';
+import { ffutils } from 'ffjavascript';
+import { ethers } from "ethers";
+import { snarkjs } from "snarkjs";
+import { WitnessCalculatorBuilder } from "circom_runtime";
+
+async function buildAnonVote(chainID, nLevels) {
+	const poseidon = await buildPoseidonReference();
+	const eddsa = await buildEddsa();
+	const babyjub = await buildBabyjub();
+	return new AnonVote(poseidon, eddsa, babyjub, chainID, nLevels);
+}
+
+// AnonVote contains all the logic to build the data structures to vote, build
+// censuses, interact with the Smart Contracts, etc.
+// To interact with the Smart Contracts, it needs a web3 gateway and a contract
+// address. For that, use the connect() method.
+class AnonVote {
+	constructor(poseidon, eddsa, babyjub, chainID, nLevels) {
+		this.poseidon = poseidon;
+		this.F = this.poseidon.F;
+		this.eddsa = eddsa;
+		this.chainID = chainID;
+		this.babyjub = babyjub;
+		this.nLevels = nLevels
+	}
+
+	// Connect the AnonVote instance to a web3 gateway and a contract address
+	async connect(web3gw, anonVotingAddress) {
+		this.web3gw = web3gw;
+
+		// This is the ABI of the AnonVoting contract
+		// If you change the contract, you need to update this ABI
+		// You can get the new ABI from the smartcontract's artifact folder
+		// (clientlib/artifacts/contracts/AnonVoting.sol/AnonVoting.json)
+		// To generate the new artifact, you need to compile the contract
+		// Run `npx hardhat compile` in the root folder of the project
+		const abi = [{"inputs":[{"internalType":"address","name":"_verifierContractAddr","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"processID","type":"uint256"},{"indexed":false,"internalType":"address","name":"creator","type":"address"},{"indexed":false,"internalType":"string","name":"topic","type":"string"},{"indexed":false,"internalType":"uint256","name":"startBlockNum","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"endBlockNum","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"censusRoot","type":"uint256"},{"indexed":false,"internalType":"uint8","name":"minTurnout","type":"uint8"},{"indexed":false,"internalType":"uint8","name":"minMajority","type":"uint8"}],"name":"NewProcess","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"processID","type":"uint256"},{"indexed":false,"internalType":"bool","name":"passed","type":"bool"}],"name":"ProcessClosed","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"processID","type":"uint256"},{"indexed":false,"internalType":"bool","name":"vote","type":"bool"}],"name":"Vote","type":"event"},{"inputs":[{"internalType":"uint256","name":"_processID","type":"uint256"},{"internalType":"uint256","name":"_nullifier","type":"uint256"}],"name":"checkIfVoted","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_processID","type":"uint256"}],"name":"closeProcess","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_processID","type":"uint256"}],"name":"isProcessPassed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastProcessID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_topic","type":"string"},{"internalType":"uint256","name":"_censusRoot","type":"uint256"},{"internalType":"uint256","name":"_startBlockNum","type":"uint256"},{"internalType":"uint256","name":"_endBlockNum","type":"uint256"},{"internalType":"uint8","name":"_minTurnout","type":"uint8"},{"internalType":"uint8","name":"_minMajority","type":"uint8"}],"name":"newProcess","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"processes","outputs":[{"internalType":"address","name":"creator","type":"address"},{"internalType":"string","name":"topic","type":"string"},{"internalType":"uint256","name":"startBlockNum","type":"uint256"},{"internalType":"uint256","name":"endBlockNum","type":"uint256"},{"internalType":"uint256","name":"censusRoot","type":"uint256"},{"internalType":"uint8","name":"minTurnout","type":"uint8"},{"internalType":"uint8","name":"minMajority","type":"uint8"},{"internalType":"uint256","name":"yesVotes","type":"uint256"},{"internalType":"uint256","name":"noVotes","type":"uint256"},{"internalType":"bool","name":"closed","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_processID","type":"uint256"},{"internalType":"bool","name":"_vote","type":"bool"},{"internalType":"uint256","name":"_nullifier","type":"uint256"},{"internalType":"uint256[2]","name":"_a","type":"uint256[2]"},{"internalType":"uint256[2][2]","name":"_b","type":"uint256[2][2]"},{"internalType":"uint256[2]","name":"_c","type":"uint256[2]"}],"name":"vote","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+		// Load the contract
+		this.anonVoting = new ethers.Contract(anonVotingAddress, abi, this.web3gw);
+	}
+
+	// Generate a BabyJubJub keypair
+	// To generate the private key, we use the users signature over "AnonVote Key Generation Secret"
+	// Then we hash the signature to get a 32-byte private key
+	// The public key is computed from the private key using the BabyJubJub curve
+	async generateKey(signer) {
+		const text = "ANONVOTE KEY GENERATION SECRET";
+		const signature = await signer.signMessage(text);
+
+		const privateKey = ethers.utils.keccak256(signature);
+
+		// Compute the public key by hashing the private key to the BabyJubJub curve
+		const publicKey = this.eddsa.prv2pub(privateKey)
+
+		// Store the private and public key
+		this.privateKey = privateKey
+		this.publicKey = [publicKey[0].toString(), publicKey[1].toString()]
+
+
+		// Compute the compressed public key
+		const compressedPublicKey = ffutils.leBuff2int(this.babyjub.packPoint(publicKey))
+		this.compressedPublicKey = ethers.utils.hexZeroPad(`0x${compressedPublicKey.toString(16)}`, 32);
+
+		return {privateKey: this.privateKey, publicKey: this.publicKey, compressedPublicKey: this.compressedPublicKey };
+	}
+
+	computeNullifier(processID) {
+		const nullifier = this.poseidon([
+			this.chainID,
+			processID,
+			this.publicKey[0],
+			this.publicKey[1],
+		]);
+		return this.F.toObject(nullifier).toString();
+	}
+
+	prepareZKInputs(processID, censusRoot, merkleproof, vote) {
+		// for this version, weight is hardcoded to 1 for all voters
+		const weight = "1";
+
+		const nullifier = this.computeNullifier(processID);
+
+		// compute signature
+		const toSign = this.poseidon([this.chainID, processID, vote]);
+		const signature = this.eddsa.signPoseidon(this.privateKey, toSign);
+
+		// set the zk-inputs
+		const inputs = {
+			chainID: this.chainID, // public inputs
+			processID: processID,
+			censusRoot: censusRoot,
+			weight: weight,
+			nullifier: nullifier,
+			vote: vote,
+			index: merkleproof.index, // private inputs
+			pubKx: this.F.toObject(this.publicKey[0]).toString(),
+			pubKy: this.F.toObject(this.publicKey[1]).toString(),
+			s: signature.S.toString(),
+			rx: this.F.toObject(signature.R8[0]).toString(),
+			ry: this.F.toObject(signature.R8[1]).toString(),
+			siblings: merkleproof.siblings,
+		};
+		return inputs;
+	}
+
+	async genZKProof(zkey, witnessCalcWasm, processID, censusRoot, merkleproof, vote) {
+		const inputs = this.prepareZKInputs(processID, censusRoot, merkleproof, vote);
+
+		let proof, publicInputs;
+		try {
+			let res = await snarkjs.groth16.fullProve(
+				inputs,
+				witnessCalcWasm,
+				zkey,
+			);
+			proof = res.proof;
+			publicInputs = res.publicSignals;
+		} catch(e) {
+			throw new Error("fullProve err: " + e);
+		}
+
+		// return public inputs + zk-proof
+		return {
+			publicInputs: {
+				chainID: publicInputs[0],
+				processID: publicInputs[1],
+				censusRoot: publicInputs[2],
+				weight: publicInputs[3],
+				nullifier: publicInputs[4],
+				vote: publicInputs[5],
+			},
+			proof: [ // proof in the SmartContract expected format
+				[proof.pi_a[0], proof.pi_a[1]],
+				[
+					[proof.pi_b[0][1], proof.pi_b[0][0]],
+					[proof.pi_b[1][1], proof.pi_b[1][0]]
+				],
+				[proof.pi_c[0], proof.pi_c[1]]
+			]
+		};
+	}
+
+	// Method to get the information of a process
+	async getProcess(processID) {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+
+		const process = await this.anonVoting.processes(processID);
+
+		// Check if the process exists
+		if (process.creator === "0x0000000000000000000000000000000000000000") {
+			return null;
+		}
+
+		// If so, return the information of the process
+		return {
+			processID: processID,
+			creator: process.creator,
+			topic: process.topic,
+			startBlock: process.startBlockNum,
+			endBlock: process.endBlockNum,
+			censusRoot: process.censusRoot,
+			minMajority: process.minMajority,
+			minTurnout: process.minTurnout,
+			closed: process.closed,
+			yesVotes: process.yesVotes,
+			noVotes: process.noVotes,
+		};
+	}
+
+	// Method to get all the processes in the AnonVoting contract
+	async getProcesses() {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+
+		// Get the lastProcessID
+		const lastProcessID = await this.anonVoting.lastProcessID();
+
+		// For each processID until lastProcessID, get the process
+		// Note that the indexing is moved by 1, so the first processID is 1
+		const processes = [];
+		for (let i = 1; i <= lastProcessID; i++) {
+			const process = await this.getProcess(i);
+			processes.push(process);
+		}
+
+		return processes;
+	}
+
+	// Method to generate a new process in the AnonVoting contract
+	async newProcess(topic, censusRoot, startBlock, endBlock, minMajority, minTurnout, signer) {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+
+		const anonVotingWithSigner = this.anonVoting.connect(signer);
+
+
+
+		await anonVotingWithSigner.newProcess(topic, censusRoot, startBlock, endBlock, minMajority, minTurnout);
+
+
+		// Return the created processID
+		return  await this.anonVoting.lastProcessID();
+	}
+
+	getMerkleProof() {}
+	getProvingKey() {}
+
+	async castVote(signer, zkey, witnessCalcWasm, processID, censusRoot, merkleproof, voteBool) {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+		const anonVotingWithSigner = this.anonVoting.connect(signer);
+
+		let vote = 0;
+		if (voteBool){
+			vote = 1;
+		}
+
+		// prepare the inputs and generate the zkproof
+		let proofAndPI = await this.genZKProof(zkey, witnessCalcWasm,
+			processID, censusRoot, merkleproof, vote);
+
+		// call contract vote method sending the proof
+		await anonVotingWithSigner.vote(processID, voteBool,
+			proofAndPI.publicInputs.nullifier, proofAndPI.proof[0],
+			proofAndPI.proof[1], proofAndPI.proof[2]);
+	}
+
+	// Method to check if a voter has already voted in a process
+	// Returns true if the voter has already voted, false otherwise
+	// TODO - create a test for this method
+	async checkIfVoted(processID) {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+
+		const nullifier = this.computeNullifier(processID);
+
+		// Check if the nullifier is in the nullifiers array
+		return await this.anonVoting.checkIfVoted(processID, nullifier);
+	}
+
+	// Method to close a process
+	// To get the results, the user must call the getProcess method
+	closeProcess(processID, signer) {
+		if (!this.web3gw) {
+			throw new Error("web3gw not defined. Use connect() first");
+		}
+
+		const anonVotingWithSigner = this.anonVoting.connect(signer);
+
+		return anonVotingWithSigner.closeProcess(processID);
+	}
+}
+
+module.exports = { AnonVote, buildAnonVote };
