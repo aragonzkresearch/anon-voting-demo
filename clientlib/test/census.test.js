f3997ae0b14bab010effd15cdc1035f86d15c700
arnaucube <root@arnaucube.com>
2023-03-02 21:03:29 +0100

    clientlib: Add methods to export & import census from & to json

--- clientlib/test/census.test.js
+++ clientlib/test/census.test.js
@@ -40,6 +40,35 @@ describe("Census generation tests", function () {
 		let proof = await census.generateProof(0);
 		assert(proof.index == 0);
 		const leafValue = av.poseidon([publicKeys[0][0], publicKeys[0][1], "1"]);
-		assert(proof.value, av.F.toObject(leafValue).toString());
+		expect(proof.value).to.equal(av.F.toObject(leafValue).toString());
+	});
+	it("export & import census", async () => {
+		const av = await buildAnonVote(chainID, nLevels, null);
+		const census = await buildCensus(nLevels);
+		
+		// simulate key generation
+		const privateKey = fromHexString(
+			"0001020304050607080900010203040506070809000102030405060708090000",
+		);
+		const publicKey = av.eddsa.prv2pub(privateKey);
+		
+		// gen other pubKeys
+		let publicKeys = [publicKey];
+		for (let i=1; i<10; i++) {
+			const privateKey = fromHexString(
+				"000102030405060708090001020304050607080900010203040506070809000"+i,
+			);
+			const publicKey = av.eddsa.prv2pub(privateKey);
+			publicKeys.push(publicKey);
+		}
+		
+		await census.addKeys(publicKeys);
+
+		let exportedJSON = census.export();
+
+		const census2 = await buildCensus(nLevels);
+		await census2.import(exportedJSON);
+
+		expect(census2.root()).to.equal(census.root());
 	});
 });
