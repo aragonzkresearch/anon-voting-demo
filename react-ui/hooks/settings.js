dc28a996c60556c27127dd1dbc8fcba7ded75bed
Craig Sailor <craigsailor@gmail.com>
2023-03-16 16:45:56 +0100

    Demo settings change (#72)
    * demo settings change
    
    * Update settings.js
    
    ---------
    
    Co-authored-by: Craig <craig>

--- react-ui/hooks/settings.js
+++ react-ui/hooks/settings.js
@@ -1,12 +1,10 @@
-const VOTING_ADDR = "0x4AC1BEE5B764521a0d919Fe4580452Ee5dc47613";
+const VOTING_ADDR = "0x51506Ad37DdA1DBD6e4429A74B4b005C87487de8";
 const IPFS_GATEWAY = "https://ipfs.io/ipfs";
 const N_LEVELS = 16;
-// For our testing
-const SIGNING_TEXT = "ANONVOTE KEY GENERATION SECRET";
 const VOTING_GAS_LIMIT = 500000;
 
 // For the demo
-//const SIGNING_TEXT = "Anonymous Voting Demo\n\nSign this message if you trust this application.";
+const SIGNING_TEXT = "Anonymous Voting Demo\n\nSign this message if you trust this application.";
 
 export {
 	VOTING_ADDR,
